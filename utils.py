adc79f0fbb780072f3fa0a597985f0b4a588e644
Joel Lappalainen <lappalj8@gmail.com>
2022-02-21 13:43:11 +0200

    Add new special operator case for tampere

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 def parse_route_id(feed, route_id, trip_id, otp_data):
     if feed == "tampere":
+        if len(route_id) > 6 and route_id[-6:] == "701871":
+            return route_id[0:-6]
         if len(route_id) > 5 and (route_id[-5:] == "47374" or route_id[-5:] == "56920"):
             return route_id[0:-5]
-        else:
-            return route_id[0:-4]
+        return route_id[0:-4]
     elif feed == "OULU":
         feed_scoped_id = "OULU:" + trip_id
         if otp_data == None or feed_scoped_id not in otp_data:
